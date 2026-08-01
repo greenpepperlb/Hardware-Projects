593a166afb75567a0911ad351e34862e089e31c1
White tea <38836086+greenpepperlb@users.noreply.github.com>
2026-08-01 21:59:33 +0200

    Delete SymCirc directory

--- SymCirc/sym2tf.m
+++
@@ -1,8 +0,0 @@
-function H = sym2tf(Hsym)
-%SYM2TF Turns a symbolic fraction in a single parameter into a TF
-[N, D] = numden(Hsym);
-Np = sym2poly(N);
-Dp = sym2poly(D);
-H  = tf(Np / Np(1), Dp / Np(1));
-end
-
